50feeba8f1475529df36d081d339395198fc4e7d
clloncon <61201379+clloncon@users.noreply.github.com>
2020-07-23 17:22:01 -0400

    Capacitacion

---
+++ src/main/java/cl/awake/segurito/model/CapacitacionRepository.java
@@ -0,0 +1,7 @@
+package cl.awake.segurito.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CapacitacionRepository extends CrudRepository<Capacitacion, Integer>{
+
+}
